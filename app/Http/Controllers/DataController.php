cf9268ec1800d39abadda2e97ca97a45701a3ff3
noenic <noenic>
2024-01-12 14:54:47 +0100

    DERNIER refactor de get_ressource_timesheet

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -35,7 +35,7 @@ class DataController extends Controller
 
         $timestamp_debut_du_mois= mktime(0, 0, 0, $request_month, 1, $request_year);
         $timestamp_fin_du_mois= mktime(23, 59, 59, $request_month, date('t', $timestamp_debut_du_mois), $request_year);
-
+        $nb_jours_dans_le_mois = date('t', $timestamp_debut_du_mois);
         $results = utilise::selectRaw('projets.nom as projectName, SUM((utilise.horodatageFinUtilisation - utilise.horodatageDebutUtilisation)/3600) as durationInHours, DAY(FROM_UNIXTIME(utilise.horodatageDebutUtilisation)) AS dayOfMonth')
             ->join('sessions', 'utilise.idSession', '=', 'sessions.id')
             ->join('projets', 'sessions.idProjet', '=', 'projets.id')
@@ -47,17 +47,34 @@ class DataController extends Controller
 
         // code sql de la requête : 
 
-        $results = $results->map(function ($item, $key) {
+        // On veut recuperer un truc du genre :
+        // {
+        //     "status": "success",
+        //     "result": [
+        //       {
+        //         "projectName": "ICNDE",
+        //         "workingDays": [] // tableau de 31 jours, chaque jour contient le nombre d'heures travaillées dans l'ordre
+        //       },
+        //       {
+        //         "projectName": "R&D Interne",
+        //         "workingDays": []
+        //       }
+        //     ]
+        //   }
+
+        $results = $results->map(function ($project) use ($nb_jours_dans_le_mois) {
+            $workingDays = [];
+            for ($i = 1; $i <= $nb_jours_dans_le_mois; $i++) {
+                $workingDays[] = "";
+            }
+            foreach ($project as $day) {
+                $workingDays[$day->dayOfMonth - 1] = $day->durationInHours;
+            }
             return [
-                'projectName' => $key,
-                'workingDays' => $item->map(function ($item, $key) {
-                    return [
-                        'dayOfMonth' => $item['dayOfMonth'],
-                        'durationInHours' => $item['durationInHours']
-                    ];
-                })->values() // Extract the values as a list
+                'projectName' => $project[0]->projectName,
+                'workingDays' => $workingDays
             ];
-        })->values(); // Extract the values as a list
+        })->values();
         
         // On affiche le body de la requête
         return sendResponse('success', $results, 200);
