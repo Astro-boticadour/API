8c46022fae68d13023855960680f2f02ce20357a
noenic <noenic>
2024-01-12 14:40:06 +0100

    refactor get_ressource_timesheet

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -33,48 +33,34 @@ class DataController extends Controller
             return sendError("Vous ne pouvez pas demander un mois dans le futur", 400);
         }
 
-        // On transforme le numero du mois en nom
-        $request_month = date('F', mktime(0, 0, 0, $request_month, 10));
+        $timestamp_debut_du_mois= mktime(0, 0, 0, $request_month, 1, $request_year);
+        $timestamp_fin_du_mois= mktime(23, 59, 59, $request_month, date('t', $timestamp_debut_du_mois), $request_year);
 
-        // On recuepre le nombre de jour dans le mois
-        $number_of_days = cal_days_in_month(CAL_GREGORIAN, explode('-', $request['month'])[0], explode('-', $request['month'])[1]);
-        
-        for ($i = 1; $i <= $number_of_days; $i++) {
-            $results[$i] = Ressource::select(
-                'ressources.nom as ressource_nom',
-                'projets.nom as projet_nom',
-                \DB::raw('SUM(utilise.horodatageFinUtilisation - utilise.horodatageDebutUtilisation) / 3600 as duree_utilisation')
-            )
-            ->join('utilise', 'ressources.id', '=', 'utilise.idRessource') // Corrected join condition
+        $results = utilise::selectRaw('projets.nom as projectName, SUM((utilise.horodatageFinUtilisation - utilise.horodatageDebutUtilisation)/3600) as durationInHours, DAY(FROM_UNIXTIME(utilise.horodatageDebutUtilisation)) AS dayOfMonth')
             ->join('sessions', 'utilise.idSession', '=', 'sessions.id')
             ->join('projets', 'sessions.idProjet', '=', 'projets.id')
-            ->where('ressources.id', $request['idRessource'])
-            ->where('utilise.horodatageDebutUtilisation', '>=', strtotime("$i $request_month $request_year"))
-            ->where('utilise.horodatageDebutUtilisation', '<=', strtotime("$i $request_month $request_year 23:59:59"))
-            ->groupBy('ressources.nom', 'projets.nom')
-            ->get();
-        }
-        $new_results = [];
-        foreach ($results as $key => $value) {
-            foreach ($value as $key2 => $value2) {
-                $new_results[$value2['projet_nom']][$key] = $value2['duree_utilisation'];
-            }
-        }
-
-        // On rajoute les autres jours a chaque projet et on met 0
-        foreach ($new_results as $key => $value) {
-            for ($i = 1; $i <= $number_of_days; $i++) {
-                if (!isset($value[$i])) {
-                    $new_results[$key][$i] = "0";
-                }
-            }
-        }
-         
+            ->where('utilise.idRessource', $request['idRessource'])
+            ->whereBetween('utilise.horodatageDebutUtilisation', [$timestamp_debut_du_mois, $timestamp_fin_du_mois])
+            ->groupBy('projectName', 'dayOfMonth')
+            ->get()->groupBy('projectName');
         
 
+        // code sql de la requête : 
 
-
+        $results = $results->map(function ($item, $key) {
+            return [
+                'projectName' => $key,
+                'workingDays' => $item->map(function ($item, $key) {
+                    return [
+                        'dayOfMonth' => $item['dayOfMonth'],
+                        'durationInHours' => $item['durationInHours']
+                    ];
+                })->values() // Extract the values as a list
+            ];
+        })->values(); // Extract the values as a list
+        
         // On affiche le body de la requête
-        return sendResponse('success', $new_results,200);
+        return sendResponse('success', $results, 200);
+
     }
 }
